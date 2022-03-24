0f1e2c4f18d64bf89d05dcc20b34a61fe31da5ac
MoriMotonari <finn.harbeke@gmail.com>
2022-03-24 01:31:41 +0100

    all_square_cols fn

--- dfcols.py
+++ dfcols.py
@@ -6,3 +6,9 @@ def grid_col(sqcol_name: str):
     if not re.match(r"\d+_[a-zA-Z]+", sqcol_name):
         raise ValueError(f"passed argument ({sqcol_name}) is probably not a square column name, like e.g. 1_slope")
     return sqcol_name.split("_")[1]
+
+def all_square_cols(col_name: str):
+    l = []
+    for i in range(25):
+        l.append(f"{i+1}_{col_name}")
+    return l
